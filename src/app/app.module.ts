1e79e4ce254e4b48307e1acdb94e7bd3ea141f68
Chanon Chokethaworn <chanon.tod@gmail.com>
2018-09-05 11:14:05 +0700

    Create Component Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicZoneComponent } from './page/public-zone/public-zone.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
+import { RegisterComponent } from './page/register/register.component';
+import { PhoneLoginComponent } from './page/phone-login/phone-login.component';
+import { PrivateZoneComponent } from './page/private-zone/private-zone.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { CaloriesComponent } from './page/calories/calories.component';
+import { FoodComponent } from './page/food/food.component';
+import { FoodListComponent } from './page/food-list/food-list.component';
+import { SleepComponent } from './page/sleep/sleep.component';
+import { WeightComponent } from './page/weight/weight.component';
+import { ProfileComponent } from './page/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicZoneComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    PhoneLoginComponent,
+    PrivateZoneComponent,
+    DashboardComponent,
+    CaloriesComponent,
+    FoodComponent,
+    FoodListComponent,
+    SleepComponent,
+    WeightComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
