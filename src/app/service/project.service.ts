147e121caddbd725f0b02ffed7370e8d37232c8f
Chanon Chokethaworn <chanon.tod@gmail.com>
2018-09-05 17:31:52 +0700

    Create Project Service

---
+++ src/app/service/project.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  _apiUrl = `${environment.hostUrl}/api/v1/project`;
+
+  constructor(private _http: HttpClient) { }
+
+  getProject(): Observable<any> {
+    return this._http.get(this._apiUrl);
+  }
+
+  addProject(body): Observable<any> {
+    return this._http.post(this._apiUrl, body,
+      { headers:
+        {
+          'Authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyQ29kZSI6Ik5vb3RpM3ciLCJ1c2VyTmFtZSI6IkNob25uaXNhIiwidXNlckxhc3ROYW1lIjoiVGhpZW1idW5kaXQiLCJ1c2VyRW1haWwiOiJ0aWV3d2FfaGFoYUBob3RtYWlsLmNvbSJ9.Gnb5Wx3jDi-6IVtGpfG64atQMaX6brBjRozitU0nlcQ',
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+  }
+
+  updateProject(body): Observable<any> {
+    return this._http.put(this._apiUrl, body,
+      { headers:
+        {
+          'Authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyQ29kZSI6Ik5vb3RpM3ciLCJ1c2VyTmFtZSI6IkNob25uaXNhIiwidXNlckxhc3ROYW1lIjoiVGhpZW1idW5kaXQiLCJ1c2VyRW1haWwiOiJ0aWV3d2FfaGFoYUBob3RtYWlsLmNvbSJ9.Gnb5Wx3jDi-6IVtGpfG64atQMaX6brBjRozitU0nlcQ',
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+  }
+
+  deleteProject(key): Observable<any> {
+    return this._http.delete(`${this._apiUrl}/${key}`);
+  }
+}
