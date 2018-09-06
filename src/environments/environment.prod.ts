90895efc406e4fc04767a78122140728396331ab
Chanon Chokethaworn <chanon.tod@gmail.com>
2018-09-06 15:24:30 +0700

    Add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  hostUrl: 'http://train-api.pnpsw.com'
+  hostUrl: 'http://train-api.pnpsw.com',
+  firebase: {
+    apiKey: 'AIzaSyAgyrksqQHHo1XpKUkgyaT8MeRzszgP0SQ',
+    authDomain: 'training-angular-6.firebaseapp.com',
+    databaseURL: 'https://training-angular-6.firebaseio.com',
+    projectId: 'training-angular-6',
+    storageBucket: 'training-angular-6.appspot.com',
+    messagingSenderId: '496445958251'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
-  hostUrl: 'http://train-api.pnpsw.com'
+  hostUrl: 'http://train-api.pnpsw.com',
+  firebase: {
+    apiKey: 'AIzaSyAgyrksqQHHo1XpKUkgyaT8MeRzszgP0SQ',
+    authDomain: 'training-angular-6.firebaseapp.com',
+    databaseURL: 'https://training-angular-6.firebaseio.com',
+    projectId: 'training-angular-6',
+    storageBucket: 'training-angular-6.appspot.com',
+    messagingSenderId: '496445958251'
+  }
 };
 
 /*
